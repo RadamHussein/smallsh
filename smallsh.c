d13d4314fddbce13dabddc4050514bc8a79fa67e
Adam Smith <smithada@eos-class.engr.oregonstate.edu>
2016-11-15 11:53:29 -0800

    basic non-built in commands working with child processes

--- smallsh.c
+++ smallsh.c
@@ -10,27 +10,13 @@
 int check_cd_command(char *command){
 	//printf("%d\n", strlen(command));
 	char cd_command[] = "cd\n";
-	
-	//check if cd has a parameter
-	if (strlen(command) > 3){
-		printf("passing control back to main\n");
-		return 0;		
+	int result = strcmp(command, cd_command);
+
+	if (result == 0){
+		return 1; 
 	}
 	else{
-		int result = strcmp(command, cd_command);
-
-		if (result == 0){
-			//printf("!! cd command received !!\n");
-			char cwd[100];
-			//printf("Home: %s\n", getenv("HOME"));
-			chdir(getenv("HOME"));
-			getcwd(cwd, sizeof(cwd));
-			//printf("%s\n", cwd);
-			return 1; 
-		}
-		else{
-			return 0; 
-		}
+		return 0; 
 	}
 }
 
@@ -39,7 +25,6 @@ int check_status_command(char *command){
 	int result = strcmp(command, status_command);
 
 	if (result == 0){
-		printf("EXIT CODE: %d\n", getenv("?"));
 		return 1;
 	}
 	else{
@@ -52,6 +37,7 @@ int check_for_comment(char characterToCheck){
 	char *ch;
 	ch = &characterToCheck;
 	int result = strcmp(ch, comment);
+	//printf("result: %d\n", result);
 	
 	if (result == 0){
 		return 1;
@@ -63,25 +49,24 @@ int check_for_comment(char characterToCheck){
 }
 
 void handle_cd(char *command){
-	//printf("%d\n", strlen(command));
 	char cd_command[] = "cd\n";
-	//printf("!! cd command received !!\n");
 	char cwd[100];
-	//printf("Home: %s\n", getenv("HOME"));
 	chdir(getenv("HOME"));
 	getcwd(cwd, sizeof(cwd));
 	//printf("%s\n", cwd);
 }
 
 void handle_status(char *command){
-
+	printf("EXIT CODE: %d\n", getenv("?"));
 }
 
 void execute_command(char *command){
 	//printf("command was: %s\n", command);
-	//printf(getenv(command));	
+	//printf("length of command was %d\n", strlen(command));
 	system(command);
 }
+
+//void spawn_child(){}
 
 int main(){
 	int x = 100;
@@ -90,33 +75,77 @@ int main(){
 	char exit_command[] = "exit\n";
 	char cd_command[] = "cd\n";
 	char status_command[] = "status\n";
+	pid_t spawnpid = -5;
+	int ten = 10;
+	int exitMethod;
 
 	while (x == 100){
+		spawnpid = -5;
+		ten = 10;	//remove later
 		printf(": ");
 		fgets(command, 20, stdin);
 		fflush(stdout);
 	
 		result = strcmp(command, exit_command);
 
+		//exit command
 		if (result == 0){
 			//user entered exit command
 			break;
 		}
-		else{
-			if (check_cd_command(command) == 0 && check_status_command(command) == 0){
-				
-				//check_status_command(command);			
-			
-				result = check_for_comment(command[0]);
+		//cd command
+		else if (check_cd_command(command) == 1){
+			handle_cd(command);
+		}
+		//status command
+		else if (check_status_command(command) == 1){
+			handle_status(command);
+		}
+		//other command
+		else{	
+			//first make sure that more than 1 character was entered
+			if (strlen(command) >= 2){
+				//next check if the first character was an #
+				result = check_for_comment(command[0]);		
 				if (result == 0){
 					//printf("%s", command);
-					fflush(stdout);
-					execute_command(command);				
+					//fflush(stdout);
+					spawnpid = fork();
+					switch (spawnpid){
+						case -1:
+							perror("Hull Breach!");
+							exit(1);
+							break;
+						case 0:
+							ten = ten + 1; 
+							printf("I am the child! ten = %d\n", ten);
+							printf("execing %s...\n", command);
+							fflush(stdout);
+							execlp(command, NULL);
+							execute_command(command);
+							exit(0);					
+
+							perror("exec");
+							return(1);
+							
+							break;
+						default:
+							ten = ten - 1;
+							//printf("I am the parent! ten = %d\n", ten);
+							pid_t exitpid = wait(&exitMethod);
+							printf("parent: child exited [%d]\n", exitMethod);
+							fflush(stdout);
+
+							//exit(0);
+							break;
+					}
+					printf("This will be executed by both of us\n");
+					//probably delete this:
+					//execute_command(command);				
 				}
 			}
-
-			fflush(stdout);	
 		}
+		fflush(stdout);	
 	};
 
 return 0;
