f7f1f7558d60a396ac6ed99c8de353141adf4d81
Adam Smith <smithada@eos-class.engr.oregonstate.edu>
2016-11-16 12:49:07 -0800

    added function to check type of command entered

--- smallsh.c
+++ smallsh.c
@@ -7,18 +7,6 @@
 #include <signal.h>
 #include <string.h>
 
-int check_cd_command(char *command){
-	//printf("%d\n", strlen(command));
-	char cd_command[] = "cd\n";
-	int result = strcmp(command, cd_command);
-
-	if (result == 0){
-		return 1; 
-	}
-	else{
-		return 0; 
-	}
-}
 
 int check_status_command(char *command){
 	char status_command[] = "status\n";
@@ -66,12 +54,91 @@ void execute_command(char *command){
 	system(command);
 }
 
-//void spawn_child(){}
+void handle_cd_relative(char *command){
+	char cwd[100];
+	char changeDir[100];
+	char result;
+	char *directory;
+
+	//get the current working directory
+	getcwd(cwd, sizeof(cwd));
+
+	//parse the directory to move to out of the cd command
+	directory = strtok(command, " ");
+	directory = strtok(NULL, " ");
+
+	//change directory
+	result = sprintf(changeDir, "%s/%s", cwd, directory);
+	//printf("changing to %s\n", changeDir);
+	chdir(changeDir);
+}
+
+int check_cd_command(char *command){
+	//printf("lenght of cd: %d\n", strlen(command));
+	char cd_command[] = "cd\n";
+	char longer_command[] = "cd";
+	char destCommand[100];
+	int result;
+	char *temp;
+
+	//copy command to new string before messing with it
+	memset(destCommand, '\0', sizeof(destCommand));
+	strcpy(destCommand, command);
+
+	//get the first 2 characters from command and compare
+	if (strlen(command) > 3){
+		temp = strtok(command, " ");
+		//printf("temp contains: %s\n", temp);
+		//printf("length of temp: %d\n", strlen(temp));
+
+		result = strcmp(temp, longer_command);
+		//printf("cd compare result: %d\n", result);
+
+		if (result == 0){
+			//printf("command contains %s\n", destCommand);
+			//execute cd command
+			handle_cd_relative(destCommand);
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+	//command contains only cd. Compare to cd_command
+	else{
+		result = strcmp(command, cd_command);
+
+		if (result == 0){
+			//execute the cd command
+			handle_cd(command);
+			return 1; 
+		}
+		else{
+			return 0; 
+		}
+	}
+}
+
+void check_command_type(char *command){
+	int i; 
+	for (i = 0; i < strlen(command); i++){
+		if (command[i] == '>'){
+			printf("> found\n");
+		}
+		else if (command[i] == '<'){
+			printf("< found\n");
+		}
+		else if (command[i] == '&'){
+			printf("& found\n");
+		}
+	}
+}
 
 int main(){
 	int x = 100;
 	int result;
 	char command[2048];
+	char tempCommand[2048];
 	char exit_command[] = "exit\n";
 	char cd_command[] = "cd\n";
 	char status_command[] = "status\n";
@@ -80,13 +147,18 @@ int main(){
 	int exitMethod;
 
 	while (x == 100){
+		Top:
 		spawnpid = -5;
 		ten = 10;	//remove later
 		printf(": ");
 		fgets(command, 20, stdin);
 		fflush(stdout);
-	
+
+		//check for exit command	
 		result = strcmp(command, exit_command);
+
+		//save a copy of the command
+		strcpy(tempCommand, command);		
 
 		//exit command
 		if (result == 0){
@@ -95,21 +167,25 @@ int main(){
 		}
 		//cd command
 		else if (check_cd_command(command) == 1){
-			handle_cd(command);
+			//skip to bottom of loop. cd command has been executed
+			goto Top;
 		}
 		//status command
 		else if (check_status_command(command) == 1){
 			handle_status(command);
 		}
 		//other command
-		else{	
+		else{
+			//restore command after getting messed up in check_cd_command()	
+			strcpy(command, tempCommand);
+
 			//first make sure that more than 1 character was entered
 			if (strlen(command) >= 2){
 				//next check if the first character was an #
 				result = check_for_comment(command[0]);		
 				if (result == 0){
-					//printf("%s", command);
 					//fflush(stdout);
+					check_command_type(command);
 					spawnpid = fork();
 					switch (spawnpid){
 						case -1:
@@ -118,7 +194,7 @@ int main(){
 							break;
 						case 0:
 							ten = ten + 1; 
-							printf("I am the child! ten = %d\n", ten);
+							//printf("I am the child! ten = %d\n", ten);
 							printf("execing %s...\n", command);
 							fflush(stdout);
 							execlp(command, NULL);
@@ -139,13 +215,11 @@ int main(){
 							//exit(0);
 							break;
 					}
-					printf("This will be executed by both of us\n");
-					//probably delete this:
-					//execute_command(command);				
+					//printf("This will be executed by both of us\n");
 				}
 			}
 		}
-		fflush(stdout);	
+		fflush(stdout);
 	};
 
 return 0;
